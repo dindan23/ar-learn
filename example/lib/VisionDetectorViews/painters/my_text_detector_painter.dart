63d51d6aabc96b593fd9da1b9e3bebf5890d81b0
emankcin <5310937+emankcin@users.noreply.github.com>
2022-02-11 16:09:28 +0100

    Achieve blinking boxes

---
+++ example/lib/VisionDetectorViews/painters/my_text_detector_painter.dart
@@ -0,0 +1,79 @@
+import 'dart:collection';
+import 'dart:ui';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+import 'coordinates_translator.dart';
+
+class MyTextDetectorPainter extends CustomPainter {
+  MyTextDetectorPainter(
+      this.recognisedText, this.absoluteImageSize, this.rotation);
+
+  final RecognisedText recognisedText;
+  final Size absoluteImageSize;
+  final InputImageRotation rotation;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.0
+      ..color = Colors.lightGreenAccent;
+
+    final Paint background = Paint()..color = Color(0x99000000);
+    final Map<String, String> dict = HashMap();
+    dict.addAll({"Abitur": "Abitur is defined", "Kapitel": "Kapitel is defined.", "Logarithmusfunktionen": "log is defined"});
+    
+    for (final textBlock in recognisedText.blocks) {
+      for (final textLine in textBlock.lines) {
+        var words = textLine.elements;
+        words.retainWhere((element) => dict.containsKey(element.text));
+        for (final textWord in words) {
+          final ParagraphBuilder builder = ParagraphBuilder(
+            ParagraphStyle(
+                textAlign: TextAlign.left,
+                fontSize: 16,
+                textDirection: TextDirection.ltr),
+          );
+          builder.pushStyle(
+              ui.TextStyle(
+                  color: Colors.lightGreenAccent, background: background));
+          builder.addText('${textWord.text}');
+          builder.pop();
+
+          final left =
+          translateX(textWord.rect.left, rotation, size, absoluteImageSize);
+          final top =
+          translateY(textWord.rect.top, rotation, size, absoluteImageSize);
+          final right =
+          translateX(textWord.rect.right, rotation, size, absoluteImageSize);
+          final bottom =
+          translateY(textWord.rect.bottom, rotation, size, absoluteImageSize);
+
+          canvas.drawRect(
+            Rect.fromLTRB(left, top, right, bottom),
+            paint,
+          );
+
+          canvas.drawParagraph(
+            builder.build()
+              ..layout(ParagraphConstraints(
+                width: right - left,
+              )),
+            Offset(left, top),
+          );
+        }
+      }
+    }
+
+  }
+
+  @override
+  bool shouldRepaint(MyTextDetectorPainter oldDelegate) {
+    // TODO: return false if 3 words in new and old text are the same
+    // TODO: actually in the case of 3 equal words sets of boxes should be merged
+    return oldDelegate.recognisedText != recognisedText;
+  }
+}
--- example/lib/VisionDetectorViews/text_detectorv2_view.dart
+++ example/lib/VisionDetectorViews/text_detectorv2_view.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:vector_math/vector_math.dart';
 
-
 import 'camera_view.dart';
-import 'painters/text_detector_painter.dart';
+import 'painters/my_text_detector_painter.dart';
 
 class TextDetectorV2View extends StatefulWidget {
   @override
@@ -12,24 +11,19 @@ class TextDetectorV2View extends StatefulWidget {
 }
 
 class _TextDetectorViewV2State extends State<TextDetectorV2View> {
-  TextDetectorV2 textDetector = GoogleMlKit.vision.textDetectorV2(
-
-  );
+  TextDetectorV2 textDetector = GoogleMlKit.vision.textDetectorV2();
   bool isBusy = false;
   CustomPaint? customPaint;
 
   String titleVar = 'NOT FOUND';
-  String wordFinder = 'Sehtest';
-  String varz='';
-
-
+  String wordFinder = 'Kapitel';
+  String varz = '';
 
   @override
   void dispose() async {
     super.dispose();
     await textDetector.close();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,22 +40,16 @@ class _TextDetectorViewV2State extends State<TextDetectorV2View> {
     if (isBusy) return;
     isBusy = true;
     final recognisedText = await textDetector.processImage(inputImage,
-        script: TextRecognitionOptions.DEVANAGIRI);
+        script: TextRecognitionOptions.DEFAULT);
     print('Found ${recognisedText.blocks.length} textBlocks');
     if (inputImage.inputImageData?.size != null &&
         inputImage.inputImageData?.imageRotation != null) {
-
-
-
-
-      final painter = TextDetectorPainter(
-
+      final painter = MyTextDetectorPainter(
           recognisedText,
           inputImage.inputImageData!.size,
           inputImage.inputImageData!.imageRotation);
 
       customPaint = CustomPaint(painter: painter);
-
     } else {
       customPaint = null;
     }
